cdf0802f779f5dfc6c62394a3c4bc85932c525d0
Harish-2k3 <hari2552003@gmail.com>
2025-08-29 21:31:32 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500 underline">
+      <p>Hello world!</p>
+      <Button variant={'default'}>Click me</Button>
+    </h1>
+    </>
+  )
+}
+
+export default App
